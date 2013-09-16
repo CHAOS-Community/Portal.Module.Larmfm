d40b69c1893f7e8b40e952c5bacad7ce8193041d
Jesper Fyhr Knudsen <Jesper@fyhr.dk>
2013-09-16 16:32:54 +0200

    added tools and searchview

--- src/app/Chaos.Portal.Module.Larmfm/View/SearchView.cs
+++ src/app/Chaos.Portal.Module.Larmfm/View/SearchView.cs
@@ -1,7 +1,53 @@
-﻿namespace Chaos.Portal.Module.Larmfm.View
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chaos.Portal.Module.Larmfm.View
 {
-    public class SearchView
+    public class SearchView : AView
     {
-         
+        private static int ProgramObjectId = 24;
+        private static Guid ProgramMetadataSchemaGuid = Guid.Parse("00000000-0000-0000-0000-0000df820000");
+
+        public SearchView() : base("Search")
+        {
+        }
+
+        public override IList<IViewData> Index(object objectsToIndex)
+        {
+            var obj = objectsToIndex as Object;
+
+            if(obj == null) new List<IViewData>();
+            if(obj.ObjectTypeID != ProgramObjectId) new List<IViewData>();
+
+            var metadata = obj.Metadatas.FirstOrDefault(item => item.MetadataSchemaGuid == ProgramMetadataSchemaGuid);
+
+            if (metadata == null) new List<IViewData>();
+
+            var data  = new SearchViewData();
+            var title =  metadata.MetadataXml.Descendants("Title").FirstOrDefault();
+
+            data.Id    = obj.Guid.ToString();
+            data.Title = title.Value;
+
+            return new[] {data};
+        }
+    }
+
+    public class SearchViewData : IViewData
+    {
+        public IEnumerable<KeyValuePair<string, string>> GetIndexableFields()
+        {
+            yield return UniqueIdentifier;
+            yield return new KeyValuePair<string, string>("Title", Title);
+        }
+
+        public KeyValuePair<string, string> UniqueIdentifier { get { return new KeyValuePair<string, string>("Id", Id); } }
+        public string Fullname { get { return "Chaos.Portal.Module.Larm.Data.SearchViewData"; } }
+
+        [Serialize]
+        public string Id { get; set; }
+
+        [Serialize]
+        public string Title { get; set; }
     }
 }
--- src/test/Chaos.Portal.Module.Larmfm.Test/View/SearchViewTest.cs
+++ src/test/Chaos.Portal.Module.Larmfm.Test/View/SearchViewTest.cs
@@ -1,7 +1,58 @@
 ﻿namespace Chaos.Portal.Module.Larmfm.Test.View
 {
+    using System;
+    using System.Collections.Generic;
+    using NUnit.Framework;
+    using Object = Chaos.Mcm.Data.Dto.Object;
+
+    [TestFixture]
     public class SearchViewTest
     {
-         
+        [Test]
+        public void Index_GivenProgramObject_ReturnViewDataWithTitle()
+        {
+            var view = new SearchView();
+            var obj  = Make_Object();
+
+            var result = (SearchViewData) view.Index(obj).First();
+
+            Assert.That(result.Id, Is.EqualTo("00000000-0000-0000-0000-000000000001"));
+            Assert.That(result.Title, Is.EqualTo("P7 MIX"));
+        }
+
+        [Test]
+        public void GetIndexableFields_GivenSearchViewData_ReturnTitleField()
+        {
+            var data = new SearchViewData
+                {
+                    Id = "00000000-0000-0000-0000-000000000001",
+                    Title = "P7 MIX"
+                };
+
+            var result = data.GetIndexableFields().ToList();
+
+            Assert.That(result.Any(item => item.Key == "Id" && item.Value == "00000000-0000-0000-0000-000000000001"),
+                        Is.True);
+            Assert.That(result.Any(item => item.Key == "Title" && item.Value == "P7 MIX"), Is.True);
+        }
+
+        private static Object Make_Object()
+        {
+            return new Object
+                {
+                    Guid = Guid.Parse("00000000-0000-0000-0000-000000000001"),
+                    ObjectTypeID = 24,
+                    Metadatas = new List<Metadata>
+                        {
+                            new Metadata
+                                {
+                                    MetadataSchemaGuid = Guid.Parse("00000000-0000-0000-0000-0000df820000"),
+                                    MetadataXml =
+                                        XDocument.Parse(
+                                            "<Larm.Program><PublicationDateTime>2012-02-21T16:03:00</PublicationDateTime><PublicationEndDateTime>2012-02-22T00:03:00</PublicationEndDateTime><PublicationChannel>DR P7 Mix</PublicationChannel><Title>P7 MIX</Title><Abstract></Abstract><Description>Musik med pop og sjæl. </Description><Publisher></Publisher><Subjects /><Contributors></Contributors><Creators><Creator><Name></Name><RoleName></RoleName><RoleID></RoleID></Creator></Creators><Locations /><Identifiers><DR.ProductionNumber></DR.ProductionNumber><DR.ArchiveNumber></DR.ArchiveNumber><SB.DomsID>c431cb1d-081a-47de-a7d4-cd4275a7063a</SB.DomsID></Identifiers></Larm.Program>")
+                                }
+                        }
+                };
+        }
     }
 }
