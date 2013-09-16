73be4ffdd04365d71642f687a85817841b8cf03b
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2013-09-16 15:50:18 +0200

    Initial commit

---
+++ src/app/Chaos.Portal.Module.Larmfm/LarmModule.cs
@@ -0,0 +1,14 @@
+﻿namespace Chaos.Portal.Module.Larmfm
+{
+    public class LarmModule : Mcm.McmModule
+    {
+        #region Implementation of IModule
+
+        protected override Core.Indexing.View.IView CreateObjectView()
+        {
+            return new View.ObjectView(PermissionManager);
+        }
+
+        #endregion
+    }
+}
