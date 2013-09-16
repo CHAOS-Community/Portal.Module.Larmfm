3c74685f2dff85aa0ab038b39c970cdd37e94f3c
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2013-09-16 19:31:49 +0200

    Search View is now loaded with the module

--- src/app/Chaos.Portal.Module.Larmfm/LarmModule.cs
+++ src/app/Chaos.Portal.Module.Larmfm/LarmModule.cs
@@ -1,5 +1,12 @@
 ﻿namespace Chaos.Portal.Module.Larmfm
 {
+    using System.Configuration;
+
+    using CHAOS.Net;
+
+    using Chaos.Portal.Core.Indexing.Solr;
+    using Chaos.Portal.Module.Larmfm.View;
+
     public class LarmModule : Mcm.McmModule
     {
         #region Implementation of IModule
@@ -7,6 +14,18 @@
         protected override Core.Indexing.View.IView CreateObjectView()
         {
             return new View.ObjectView(PermissionManager);
+        }
+
+        public override void Load(Core.IPortalApplication portalApplication)
+        {
+            base.Load(portalApplication);
+
+            var searchView = new SearchView();
+            searchView.WithPortalApplication(portalApplication);
+            searchView.WithCache(portalApplication.Cache);
+            searchView.WithIndex(new SolrCore(new HttpConnection(ConfigurationManager.AppSettings["SOLR_URL"]), "larm-search" ));
+
+            portalApplication.ViewManager.AddView(searchView);
         }
 
         #endregion
