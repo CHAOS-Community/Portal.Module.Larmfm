a2f32dda8d4382ec342841ef2f2561631c8ebe11
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2015-03-26 18:35:19 +0100

    Oops hardcoded path in build file

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 
  
-C:\Users\JesperFyhr\Desktop\repository\Portal\tools\nant-0.92\bin\nant.exe -buildfile:build\Portal.Module.Larmfm.build
+tools\nant-0.92\bin\nant.exe -buildfile:build\Portal.Module.Larmfm.build
